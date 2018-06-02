b397c6a71e7f93915eeea24ab41140c0c4921a09
Janus2585 <bd.yhome@gmail.com>
2018-06-02 00:10:49 -0500

    keys test

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://golden:ratio1.618@ds033066.mlab.com:33066/devconnector"
+  mongoURI: "mongodb://golden:ratio1.618@ds033066.mlab.com:33066/devconnector",
+  test: ""
 };
